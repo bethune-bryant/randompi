4301583c2ee141d4185f60ffd2c145394b2a37e7
Bryant Nelson <bryantstutoringservice@gmail.com>
2017-03-14 13:46:17 -0500

    Adding documentation.

--- RandomPI/Program.cs
+++ RandomPI/Program.cs
@@ -5,12 +5,20 @@ using System.Threading.Tasks;
 
 namespace RandomPI
 {
+    /// <summary>
+    /// Computes PI as described here:
+    /// https://www.youtube.com/watch?v=RZBhSi_PwHU
+    /// Using the fact that the probability of any 2 numbers being coprime is 6/(pi^2).
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            //The number of seconds to compute samples.
             int runtime = 10; if (args.Count() > 0) runtime = int.Parse(args[0]);
+            //The number of threads to run on. By default all available threads.
             int threads = Environment.ProcessorCount; if (args.Count() > 1) threads = int.Parse(args[1]);
+            //The random generator seed. (pi by deafult!)
             int seed = 31415926; if (args.Count() > 2) seed = int.Parse(args[2]);
 
             ConcurrentBag<int> coPrimeCounts = new ConcurrentBag<int>();
@@ -18,17 +26,15 @@ namespace RandomPI
             Parallel.For(0, threads, delegate (int i)
             {
                 DateTime start = DateTime.Now;
-
                 Random rand = new Random(seed + i);
 
                 int count = 0;
                 int coPrimes = 0;
-                while ((DateTime.Now - start).TotalSeconds < runtime)
+                while ((DateTime.Now - start).TotalSeconds < runtime) //Find random samples for <runtime> seconds.
                 {
                     count++;
                     if (Utilities.IsCoPrime(Utilities.RandomLong(rand), Utilities.RandomLong(rand))) coPrimes++;
                 }
-
                 totalCounts.Add(count);
                 coPrimeCounts.Add(coPrimes);
             });
@@ -46,6 +52,12 @@ namespace RandomPI
 
     static class Utilities
     {
+        /// <summary>
+        /// Returns the greatest common divisor of two unsigned long values. 
+        /// </summary>
+        /// <param name="a">First Value</param>
+        /// <param name="b">Second Value</param>
+        /// <returns>The greatest common divisor of two unsigned long values.</returns>
         public static ulong GCD(ulong a, ulong b)
         {
             ulong Remainder;
@@ -60,11 +72,23 @@ namespace RandomPI
             return a;
         }
 
+        /// <summary>
+        /// Determines whether or not two unsigned long values are coprime. 
+        /// Meaning their greatest common divisor is 1.
+        /// </summary>
+        /// <param name="a">First Value</param>
+        /// <param name="b">Second Value</param>
+        /// <returns>True if the two values are coprime, false otherwise.</returns>
         public static bool IsCoPrime(ulong a, ulong b)
         {
             return GCD(a, b) == 1;
         }
 
+        /// <summary>
+        /// Uses an input random number generator to generate a random unsigned long value.
+        /// </summary>
+        /// <param name="rand">Random number generator.</param>
+        /// <returns>Random unsigned long.</returns>
         public static ulong RandomLong(Random rand)
         {
             byte[] buf = new byte[8];
